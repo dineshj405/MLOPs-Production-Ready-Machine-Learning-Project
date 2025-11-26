52d47e2a02f67473d8d0c895fa9bba2d0d317d6c
Dinesh Jamdade <dineshj405@gmail.com>
2025-11-26 09:12:08 +0530

    requirements file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="DJ",
+    author_email="dineshj405@gmail.com",
+    packages=find_packages()
+)
